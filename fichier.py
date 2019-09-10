419548b46414290e685179669936eefa63a65ecf
Madeleine Diop <leinamadou95@gmail.com>
2019-09-10 19:42:03 +0000

    ajout d'une fonction qui permet de remplir un dictionnaire

--- fichier.py
+++ fichier.py
@@ -4,6 +4,17 @@ def list_bidimensionnelle(A):
     w=[sum(x) for x in A]
     B.append(w)
     return(B)
-
+    
 A=[[1,2,3],[0,7,5],[10,4,12]]
 print(list_bidimensionnelle(A))
+
+def remplirDict():
+    i=1
+    d=dict()
+    n=int(input("donner le nbre de matiére"))
+    while i<=n:
+        m=(input("le nom de la matiére: "))
+        x=int(input("la note obtenue: "))
+        d[m]=x
+        i += 1
+    return d
